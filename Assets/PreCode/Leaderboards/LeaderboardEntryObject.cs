9e82d68c3c514a79d45ad455016ccec1968dd053
Jared Slawski <jaredslawski@gmail.com>
2024-10-03 20:41:14 -0600

    Ludum Pre Code

---
+++ Assets/PreCode/Leaderboards/LeaderboardEntryObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.Networking;
+
+public class LeaderboardEntryObject : MonoBehaviour
+{
+    public TextMeshProUGUI username;
+    [SerializeField]
+    private TextMeshProUGUI scoreText;
+
+    private void Start()
+    {
+        if (this.scoreText.text == "0")
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    public void UpdateEntry(string username, float score)
+    {
+        this.gameObject.SetActive(true);
+        
+        if (username == "" || score == 0)
+        {            
+            return;
+        }
+        
+        this.username.text = username;
+        this.scoreText.text = Mathf.RoundToInt(score).ToString();
+    }    
+}
---
+++ Assets/PreCode/Leaderboards/LeaderboardManager.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class LeaderboardUpdate
+{
+    public string username;
+    public float value;
+    public string tableName;
+
+    public LeaderboardUpdate(string newName, float newValue, string tableName)
+    {
+        this.username = newName;
+        this.value = newValue;
+        this.tableName = tableName;
+    }
+}
+
+public class LeaderboardManager : MonoBehaviour
+{
+    public static LeaderboardManager instance;
+
+    [HideInInspector]
+    public LeaderboardEntryObject[] leaderboardEntryObjects;
+
+    private Queue<LeaderboardUpdate> queuedUpdates;
+
+    private bool readyToProcessUpdate = true;
+
+    public LeaderboardData currentLeaderboardData;
+
+    [SerializeField]
+    private GameObject leaderboardObject;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+
+        this.leaderboardEntryObjects = GetComponentsInChildren<LeaderboardEntryObject>(true);
+        this.queuedUpdates = new Queue<LeaderboardUpdate>();
+    }
+
+    public void DisableLeaderboard()
+    {
+        this.leaderboardObject.SetActive(false);
+    }
+
+    public void RefreshLeaderboard(string tableName)
+    {
+        this.RequestLeaderboard(tableName);
+    }
+
+    #region Get Leaderboard
+    private void RequestLeaderboard(string tableName)
+    {
+        GetCabbageLeaderboardAsyncRequest request = new GetCabbageLeaderboardAsyncRequest(tableName, this.RequestLeaderboardSuccess, this.RequestLeaderboardFailure);
+        request.Send();
+    }
+
+    private void RequestLeaderboardSuccess(string data)
+    {
+        //Empty leaderboard, return
+        if (data == "[]")
+        {
+            return;
+        }
+
+        this.currentLeaderboardData = JsonUtility.FromJson<LeaderboardData>(data);
+
+        if (this.leaderboardObject.activeSelf == true)
+        {
+            this.UpdateLeaderboardVisuals();
+        }
+    }
+
+    private void RequestLeaderboardFailure()
+    {
+        Debug.LogError("Error: Unable to get leaderboard");
+    }
+    #endregion
+
+    #region UpdateLeaderboard
+    private void UpdateLeaderboard(LeaderboardUpdate updateValues)
+    {
+        UpdateCabbageLeaderboardAsyncRequest request = new UpdateCabbageLeaderboardAsyncRequest(updateValues.username, updateValues.value.ToString(), updateValues.tableName, this.UpdateLeaderboardSuccess, this.UpdateLeaderboardFailure);
+        request.Send();
+    }
+
+    private void UpdateLeaderboardSuccess(string data)
+    {
+        this.currentLeaderboardData = JsonUtility.FromJson<LeaderboardData>(data);
+        this.UpdateLeaderboardVisuals();
+        this.readyToProcessUpdate = true;
+    }
+
+    private void UpdateLeaderboardFailure()
+    {
+        Debug.LogError("Error: Unable to update leaderboard entry");
+    }
+    #endregion
+
+    public LeaderboardEntryData GetTopPlayer()
+    {
+        if (this.currentLeaderboardData.entries.Count > 0)
+        {
+            return this.currentLeaderboardData.entries[0];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void QueueLeaderboardUpdate(string username, float value, string tableName)
+    {
+        this.queuedUpdates.Enqueue(new LeaderboardUpdate(username, value, tableName));
+    }
+
+    private void FixedUpdate()
+    {
+        if (this.queuedUpdates.Count > 0 && this.readyToProcessUpdate == true)
+        {
+            this.ProcessUpdate(this.queuedUpdates.Dequeue());
+        }
+    }
+
+    private void ProcessUpdate(LeaderboardUpdate updateValues)
+    {
+        this.readyToProcessUpdate = false;
+
+        this.UpdateLeaderboard(updateValues);
+    }
+
+    private void UpdateLeaderboardVisuals()
+    {
+        this.ClearLeaderboardVisuals();
+
+        for (int i = 0; i < this.currentLeaderboardData.entries.Count && i < this.leaderboardEntryObjects.Length; i++)
+        {
+            this.leaderboardEntryObjects[i].UpdateEntry(this.currentLeaderboardData.entries[i].username, this.currentLeaderboardData.entries[i].value);
+        }
+    }
+
+    private void ClearLeaderboardVisuals()
+    {
+        for (int i = 0; i < this.leaderboardEntryObjects.Length; i++)
+        {
+            this.leaderboardEntryObjects[i].UpdateEntry(string.Empty, 0);
+        }
+    }
+
+    public bool IsTopPlayer(string username)
+    {
+        return (this.GetTopPlayer().username == username);
+    }
+}
