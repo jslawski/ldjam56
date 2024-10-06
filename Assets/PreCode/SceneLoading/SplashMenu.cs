a463761de502a92d714dbdf36d4e23ab45311905
boragenel <boragenel@gmail.com>
2024-10-06 21:40:00 +0300

    Splash Screen

--- Assets/PreCode/SceneLoading/SplashMenu.cs
+++ Assets/PreCode/SceneLoading/SplashMenu.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class SplashMenu : MonoBehaviour
 {
@@ -10,15 +13,32 @@ public class SplashMenu : MonoBehaviour
 
     [SerializeField]
     private string nextSceneName;
-
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+    [SerializeField]
+    private Image skipFiller;
     private bool loading = false;
+    private bool isSkipping = false;
+    [SerializeField]
+    private float skipKeyHoldDuration = 1.2f;
+    public float fadeOutDelay = 8f;
+    public float skipTimer = 0f;
+    private bool skipped = false;
+    
     
     private void Awake()
     {
         this.fadePanel.OnFadeSequenceComplete += this.DisplaySplashScreen;
-        this.fadePanel.FadeFromBlack();
+        
+        videoPlayer.started += OnVideoStarted;
     }
 
+    void OnVideoStarted(VideoPlayer inVideoPlayer)
+    {
+        videoPlayer.started -= OnVideoStarted;
+        this.fadePanel.FadeFromBlack();
+    }
+    
     private void DisplaySplashScreen()
     {
         this.fadePanel.OnFadeSequenceComplete -= this.DisplaySplashScreen;
@@ -27,8 +47,46 @@ public class SplashMenu : MonoBehaviour
 
     private IEnumerator DisplayCoroutine()
     {
-        yield return new WaitForSeconds(3.0f);
-
+        yield return new WaitForSeconds(fadeOutDelay);
         SceneLoader.instance.LoadScene(this.nextSceneName);
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetMouseButtonDown(0))
+        {
+            isSkipping = true;
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Escape) || Input.GetMouseButtonUp(0))
+        {
+            skipFiller.fillAmount = 0;
+            isSkipping = false;
+            skipTimer = 0f;
+        }
+
+        if (isSkipping && !skipped)
+        {
+            skipTimer += Time.deltaTime;
+            skipFiller.fillAmount = skipTimer / skipKeyHoldDuration;
+            if (skipTimer > skipKeyHoldDuration)
+            {
+                SceneLoader.instance.LoadScene(this.nextSceneName);
+                StartCoroutine(FadeOutVideoVolume());
+                skipped = true;
+
+            }
+        }
+    }
+
+    public IEnumerator FadeOutVideoVolume()
+    {
+        AudioSource aSource = videoPlayer.GetTargetAudioSource(0);
+        while (aSource.volume > 0f)
+        {
+            aSource.volume -= Time.deltaTime * 0.01f;
+            yield return 0;
+        }
+    }
+    
 }
