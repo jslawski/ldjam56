7532f00c339c99c0a1a455fb22d0db9e51bf5ef8
Jared Slawski <jaredslawski@gmail.com>
2024-10-06 22:35:21 -0600

    Changed settings to play in background and improved food spawning

--- Assets/_Scripts/FoodSpawner.cs
+++ Assets/_Scripts/FoodSpawner.cs
@@ -22,10 +22,17 @@ public class FoodSpawner : MonoBehaviour
     private AnimationCurve expandCurve;
 
     private float currentExpansionTime = 0.0f;
-    private float totalExpansionTimeInSeconds = 120;  //5 min
+    private float totalExpansionTimeInSeconds = 120;  //2 min
 
-    private float minSize = 1.5f;
+    private float minSize = 2.0f;
     private float maxSize = 4.0f;
+
+    private Vector3 lastSpawnLocation = Vector3.zero;
+
+    private float minXDistanceFromOtherObject = 0.9f;
+    private float minZDistanceFromOtherObject = 0.9f;
+
+    private int maxNumberRetries = 20;
 
     private void Start()
     {
@@ -73,12 +80,10 @@ public class FoodSpawner : MonoBehaviour
 
     private void SpawnFood()
     {
-        float xValue = Random.Range(this.minFieldX, this.maxFieldX);
-        float zValue = Random.Range(this.minFieldZ, this.maxFieldZ);
-
-        Vector3 worldSpaceGenerationPosition = new Vector3(xValue, this.transform.position.y, zValue);
+        Vector3 chosenSpawnPoint = this.GetRandomSpawnPoint();   
+    
         GameObject objectToGenerate = this.foodPrefabs[Random.Range(0, this.foodPrefabs.Length)];
-        GameObject objectInstance = Instantiate(objectToGenerate, worldSpaceGenerationPosition, new Quaternion(), this.transform) as GameObject;
+        GameObject objectInstance = Instantiate(objectToGenerate, chosenSpawnPoint, new Quaternion(), this.transform) as GameObject;
 
         GrabbableObject grabbableInstance = objectInstance.GetComponentInChildren<GrabbableObject>();
 
@@ -89,5 +94,33 @@ public class FoodSpawner : MonoBehaviour
         float randomZRotation = Random.Range(0.0f, 360);
 
         grabbableInstance.gameObject.transform.rotation = Quaternion.Euler(randomXRotation, randomYRotation, randomZRotation);
+
+        this.lastSpawnLocation = chosenSpawnPoint;
+    }
+
+    private Vector3 GetRandomSpawnPoint()
+    {
+        Vector3 returnVector = this.gameObject.transform.position;
+    
+        for (int i = 0; i < this.maxNumberRetries; i++)
+        {
+            float xValue = Random.Range(this.minFieldX, this.maxFieldX);
+            float zValue = Random.Range(this.minFieldZ, this.maxFieldZ);
+
+            if (Mathf.Abs(this.lastSpawnLocation.x - xValue) <= this.minXDistanceFromOtherObject &&
+                    Mathf.Abs(this.lastSpawnLocation.z - zValue) <= this.minZDistanceFromOtherObject)
+            {
+                xValue = Random.Range(this.minFieldX, this.maxFieldX);
+                zValue = Random.Range(this.minFieldZ, this.maxFieldZ);
+            }
+            else
+            {
+                returnVector = new Vector3(xValue, this.transform.position.y, zValue);                
+
+                break;
+            }
+        }
+
+        return returnVector;
     }
 }
